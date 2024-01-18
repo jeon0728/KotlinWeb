e4e318213145325be64b1244adfbff979976031c
jeon0728 <jeon0728@gmail.com>
2024-01-18 13:00:14 +0900

    2024.01.18 THU - 비밀번호 인코딩 - companion object - BlogApplication.kt 사용중지

--- src/main/kotlin/com/example/KotlinWeb/BlogApplication.kt
+++ src/main/kotlin/com/example/KotlinWeb/BlogApplication.kt
@@ -4,10 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-@EnableConfigurationProperties(BlogProperties::class)
-class DemoApplication
-
-fun main(args: Array<String>) {
-    runApplication<DemoApplication>(*args)
-}
+//@SpringBootApplication
+//class DemoApplication
+//
+//fun main(args: Array<String>) {
+//    runApplication<DemoApplication>(*args)
+//}
--- src/main/kotlin/com/example/KotlinWeb/KotlinWebApplication.kt
+++ src/main/kotlin/com/example/KotlinWeb/KotlinWebApplication.kt
@@ -5,14 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.cglib.core.Local
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
 
-//@SpringBootApplication
-//class KotlinWebApplication
-//
-//fun main(args: Array<String>) {
-//	runApplication<KotlinWebApplication>(*args)
-//}
+@SpringBootApplication
+@EnableConfigurationProperties(BlogProperties::class)
+class KotlinWebApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinWebApplication>(*args)
+}
